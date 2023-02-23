ca11d1ba0f0fa92484843db48ac85ff711031e48
Lily ibrahimi <98154290+Lily-Ibrahimi77@users.noreply.github.com>
2023-02-23 21:27:25 +0100

    Update BugCard.js
    functions like add and delete works, the priority controls the color of the title of each bug. The solve toggle when adding shows if the bug is solved or not

--- src/components/Card/BugCard.js
+++ src/components/Card/BugCard.js
@@ -3,26 +3,115 @@ import React from "react";
 import "./bugcard.css";
 
 function BugCard() {
+  const [bugs, setBugs] = useState([]);
+  const [newBug, setNewBug] = useState({
+    title: "",
+    description: "",
+    priority: 1,
+    solved: false
+  });
+   const handleAddBug = () => {
+    setBugs([...bugs, newBug]);
+    setNewBug({
+      title: "",
+      description: "",
+      priority: 1,
+      solved: false
+    });
+  };
+
+  const handleDeleteBug = (index) => {
+    const newBugs = [...bugs];
+    newBugs.splice(index, 1);
+    setBugs(newBugs);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewBug({ ...newBug, [name]: value });
+  };
+
+  const handlePriorityChange = (event) => {
+    const { value } = event.target;
+    setNewBug({ ...newBug, priority: Number(value) });
+  };
+
+  const handleSolvedChange = (event) => {
+    const { checked } = event.target;
+    setNewBug({ ...newBug, solved: checked });
+  };
+
+  const getBugTitleColor = (priority) => {
+    if (priority === 1) {
+      return "orange";
+    } else if (priority === 2) {
+      return "green";
+    } else {
+      return "red";
+    }
+  };
+
   return (
-    <div className="add-issue">
-      <form>
-        <label>
-          Description
-          <input type="text" placeholder="Description of Issue..." />
-        </label>
+    <div className="card">
+      <h1>Bugs</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleAddBug();
+        }}
+      >
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={newBug.title}
+          onChange={handleChange}
+          required
+        />
 
-        <label>
-          Priority
-          <select name="priority" id="priority">
-            <option value="Low">Low</option>
-            <option value="Medium">Medium</option>
-            <option value="High">High</option>
-          </select>
-        </label>
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          value={newBug.description}
+          onChange={handleChange}
+          required
+        />
 
-        <button type="submit">Add</button>
-        <button type="submit">Delete</button>
+        <label htmlFor="priority">Priority</label>
+        <select
+          id="priority"
+          name="priority"
+          value={newBug.priority}
+          onChange={handlePriorityChange}
+        >
+          <option value="1">Low</option>
+          <option value="2">Normal</option>
+          <option value="3">Critical</option>
+        </select>
+
+        <label htmlFor="solved">Solved</label>
+        <input
+          type="checkbox"
+          id="solved"
+          name="solved"
+          checked={newBug.solved}
+          onChange={handleSolvedChange}
+        />
+
+        <button type="submit">Add Bug</button>
       </form>
+
+      {bugs.map((bug, index) => (
+        <div className="bug" key={index}>
+          <h2 style={{ color: getBugTitleColor(bug.priority) }}>{bug.title}</h2>
+          <p>{bug.description}</p>
+          <p>Priority: {bug.priority}</p>
+          <p>Solved: {bug.solved ? "Yes" : "No"}</p>
+          <button onClick={() => handleDeleteBug(index)}>Delete Bug</button>
+        </div>
+      ))}
     </div>
   );
 }
