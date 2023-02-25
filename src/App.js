e57aebdeedaca2175fe47cf64ee2f2d410813d61
rezan83 <rezan.moh83@gmail.com>
2023-02-25 15:30:14 +0100

    better charts styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import BugsList from './components/BugsList/BugsList';
 import BugForm from './components/BugForm';
@@ -6,18 +6,18 @@ import BugForm from './components/BugForm';
 import BugsPeriorityPie from './components/BugsPriorityPie';
 import BugsSolvedPie from './components/BugsSolvedPie';
 import Charts from './components/Charts';
-// import { bugsData } from './bugsData';
+import { bugsData } from './bugsData';
 import NavBar from './components/NavBar/NavBar';
 import { usePopulateCharts } from './hooks/usePopulateCharts';
 import { useFetchAllBugs } from './hooks/useFetchAllBugs';
 //the Issues is going to load when we click on a btn , I made it here just to see the style
 function App() {
  ;
-  // const [bugsDataSate, setBugsDataSate] = useState([]);
-  // useEffect(() => {
-  //   setBugsDataSate(bugsData);
-  // }, []);
-  const { fetchingState, setBugsDataSate, bugsDataSate } = useFetchAllBugs();
+  const [bugsDataSate, setBugsDataSate] = useState([]);
+  useEffect(() => {
+    setBugsDataSate(bugsData);
+  }, []);
+  // const { fetchingState, setBugsDataSate, bugsDataSate } = useFetchAllBugs();
   const { priorityData, solvedData } = usePopulateCharts(bugsDataSate);
   const handleGlobalChange = editedBug => {
     let oldBugIndex = bugsDataSate.findIndex(bug => bug.id === editedBug.id);
@@ -44,18 +44,18 @@ function App() {
           path="/report"
           element={
             <>
-              {fetchingState.isLoading && <h1>Loading </h1>}
+              {/* {fetchingState.isLoading && <h1>Loading </h1>}
               {fetchingState.isError && <h1>Error </h1>}
-              {fetchingState.isReady && (
+              {fetchingState.isReady && ( */}
                 <>
                   <BugForm bugsDataSate={bugsDataSate} setBugsDataSate={setBugsDataSate} />
                   <BugsList
                     bugsDataSate={bugsDataSate}
                     setBugsDataSate={setBugsDataSate}
                     handleGlobalChange={handleGlobalChange}
-                  />{' '}
+                  />
                 </>
-              )}
+              
             </>
           }
         />
