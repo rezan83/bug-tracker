4e3ee6855941cf0c35175550f52099c603b6f391
rezan83 <rezan.moh83@gmail.com>
2023-02-25 20:54:29 +0100

    added loading images, back to static file for too much api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // uncomment useFetchAllBugs related and comment bugsData to test remot api
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import BugsList from './components/BugsList/BugsList';
 import BugForm from './components/BugForm';
+// import errorImage from './images/error.svg';
+// import loadingImage from './images/loading.svg';
 
 import BugsPeriorityPie from './components/Charts/BugsPriorityPie';
 import BugsSolvedPie from './components/Charts/BugsSolvedPie';
@@ -11,20 +13,19 @@ import Charts from './components/Charts';
 import NavBar from './components/NavBar/NavBar';
 import { usePopulateCharts } from './hooks/usePopulateCharts';
 // uncomment useFetchAllBugs related and comment bugsData to test remot api
-// import { bugsData } from './bugsData';
+import { bugsData } from './bugsData';
 // uncomment useFetchAllBugs related and comment bugsData to test remot api
-import { useFetchAllBugs } from './hooks/useFetchAllBugs';
+// import { useFetchAllBugs } from './hooks/useFetchAllBugs';
 function App() {
+  // uncomment useFetchAllBugs related and comment bugsData to test remot api
+  const [bugsDataSate, setBugsDataSate] = useState([]);
+  // uncomment useFetchAllBugs related and comment bugsData to test remot api
+  useEffect(() => {
+    setBugsDataSate(bugsData);
+  }, []);
 
   // uncomment useFetchAllBugs related and comment bugsData to test remot api
-  // const [bugsDataSate, setBugsDataSate] = useState([]);
-  // uncomment useFetchAllBugs related and comment bugsData to test remot api
-  // useEffect(() => {
-  //   setBugsDataSate(bugsData);
-  // }, []);
-
-  // uncomment useFetchAllBugs related and comment bugsData to test remot api
-  const { fetchingState, setBugsDataSate, bugsDataSate } = useFetchAllBugs();
+  // const { fetchingState, setBugsDataSate, bugsDataSate } = useFetchAllBugs();
   const { priorityData, solvedData, solvedBy } = usePopulateCharts(bugsDataSate);
   const handleGlobalChange = editedBug => {
     let oldBugIndex = bugsDataSate.findIndex(bug => bug.id === editedBug.id);
@@ -43,15 +44,24 @@ function App() {
           path="/"
           element={
             <>
-              {fetchingState.isLoading && <h1>Loading </h1>}
-              {fetchingState.isError && <h1>Error </h1>}
-              {fetchingState.isReady && (
-              <Charts>
-                <BugsPeriorityPie priorityData={priorityData} />
-                <BugsSolvedPie solvedData={solvedData} />
-                <BugsSolvedByAssigneeBar solvedBy={solvedBy} />
-              </Charts>
-              )} 
+              {/* {
+              fetchingState.isLoading && (
+                <div className="loading">
+                  <img src={loadingImage} alt="loading" />
+                </div>
+              )} */}
+              {/* {fetchingState.isError && (
+                <div className="loading fetch-error">
+                  <h1>Sorry, please tray again later </h1> <img src={errorImage} alt="fetch data error" />
+                </div>
+              )} */}
+              {/* {fetchingState.isReady && ( */}
+                <Charts>
+                  <BugsPeriorityPie priorityData={priorityData} />
+                  <BugsSolvedPie solvedData={solvedData} />
+                  <BugsSolvedByAssigneeBar solvedBy={solvedBy} />
+                </Charts>
+              {/* )} */}
             </>
           }
         />
